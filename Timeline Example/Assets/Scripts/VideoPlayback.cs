b8cd2c28af105b33d0a4fe81f86a7e54a020629d
opatera <31523899+opatera@users.noreply.github.com>
2017-11-06 16:14:06 -0800

    Unity Project

---
+++ Timeline Example/Assets/Scripts/VideoPlayback.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoPlayback : MonoBehaviour {
+
+    public GameObject TV;
+    public MovieTexture movie;
+    private AudioSource audio;
+
+
+    // Use this for initialization
+    void Start () {
+
+        MovieTexture movie = (MovieTexture)TV.GetComponent<Renderer>().material.mainTexture;
+
+        audio = GetComponent<AudioSource>();
+        audio.clip = movie.audioClip;
+        movie.Play();
+        audio.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space) && movie.isPlaying)  // Used to pause video playback with the spacebar
+        {
+            movie.Pause();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Space) && !movie.isPlaying) // Used to play the video after it has been paused
+        {
+            movie.Play();
+        }
+
+    }
+}
---
+++ Timeline Example/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+public class Class1
+{
+    public Class1()
+    {
+
+        SqlConnection myConnection = new SqlConnection("user id=root;" +
+                                                       "password=team7;" +
+                                                       "server=localhost;" +
+                                                       "Trusted_Connection=yes;" +
+                                                       "database=omeka1; " +
+                                                       "connection timeout=30");
+
+
+        try
+        {
+            myConnection.Open();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        try
+        {
+            SqlDataReader myReader = null;
+            SqlCommand myCommand = new SqlCommand("select * from omeka_element_texts", myConnection);
+            myReader = myCommand.ExecuteReader();
+            while (myReader.Read())
+            {
+                Console.WriteLine(myReader["Column1"].ToString());
+                Console.WriteLine(myReader["Column2"].ToString());
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+
+        try
+        {
+            myConnection.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+}
