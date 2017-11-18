d8551e36d73d5d4b92297b89e5ca9d8bc682c7c0
Nash Wuthrich <nwuthrich@zagmail.gonzaga.edu>
2017-11-17 18:28:09 -0800

    Working prototype with movement controls

---
+++ Timeline Prototype/Assets/Scripts/onItemSelect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onItemSelect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//Debug.Log("Loaded");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+
+	void OnMouseDown()
+	{
+#pragma warning disable CS0618 // Type or member is obsolete
+        Application.LoadLevel("DetailScreen");
+#pragma warning restore CS0618 // Type or member is obsolete
+        Debug.Log("It Works");
+	}
+}
